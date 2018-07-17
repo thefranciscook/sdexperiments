b700f46350b92c193bf517fe06c849e978047f7e
Szilárd Szakács <szakacsszilard@gmail.com>
2018-07-16 22:24:22 -0400

    random points support for dynamic

--- point cloud/app/main.js
+++ point cloud/app/main.js
@@ -24,7 +24,7 @@ var getCSV  = function (url, callback) {
 }
 
 //var csv is the CSV file with headers
-function csvJSON(csv){
+var csvJSON = function(csv){
 
   var lines=csv.split("\n");
 
@@ -49,12 +49,21 @@ function csvJSON(csv){
   return JSON.stringify(result); //JSON
 }
 
-function appendPointCloudToDOM(data){
+function appendPointCloudToDOM(data, colorPreset){
+
+	var c = colorPreset;
+	if(!c){
+		c = "Cool";
+	}
+
+	console.log(c);
+
 	var pointCloudElement = document.createElement("a-scatterplot");
 	pointCloudElement.setAttribute('x', "Y");
 	pointCloudElement.setAttribute('y', "Z");
 	pointCloudElement.setAttribute('z', "X");
 	pointCloudElement.setAttribute('val', "SIGNAL_STRENGTH");
+	//TODO colorpreset support
 	pointCloudElement.setAttribute('colorpreset', "cool");
 	pointCloudElement.setAttribute('showcolorbar', "false");
 	
@@ -75,6 +84,10 @@ function appendPointCloudToDOM(data){
 	pointCloudParent.appendChild(pointCloudElement); 
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 
 var pointCloudId = window.location.hash.substr(1);
 
@@ -89,49 +102,108 @@ const client = contentful.createClient({
   accessToken: ACCESS_TOKEN
 })
 
-if(pointCloudId){
-// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
-client.getEntry(pointCloudId).then(
-	function(pointCloudEntry){
+var loadPointCloud = function(){
 
-		console.log(pointCloudEntry);
+	if(pointCloudId){
+	// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+	client.getEntry(pointCloudId).then(
+		function(pointCloudEntry){
 
-		var pointCloudData = {
-						scaleX: pointCloudEntry.fields.scaleX,
-						scaleY: pointCloudEntry.fields.scaleY,
-						scaleZ: pointCloudEntry.fields.scaleZ,
-						positionX: pointCloudEntry.fields.positionX,
-						positionY: pointCloudEntry.fields.positionY,
-						positionZ: pointCloudEntry.fields.positionZ,
-					};
+			console.log(pointCloudEntry);
 
-		var jsonFile = pointCloudEntry.fields.jsonFile;
-		var csvFile = pointCloudEntry.fields.csvFile;
+			var pointCloudData = {
+							scaleX: pointCloudEntry.fields.scaleX,
+							scaleY: pointCloudEntry.fields.scaleY,
+							scaleZ: pointCloudEntry.fields.scaleZ,
+							positionX: pointCloudEntry.fields.positionX,
+							positionY: pointCloudEntry.fields.positionY,
+							positionZ: pointCloudEntry.fields.positionZ,
+						};
 
-		// if(jsonFile){
-		// 	client.getAsset(jsonFile.sys.id)
-		// 		.then(function(jsonFileAsset){ 
-		// 			console.log(jsonFileAsset);
-		// 			pointCloudData.jsonUrl = jsonFileAsset.fields.file.url;
-		// 			appendPointCloudToDOM(pointCloudData);
-		// 		})
-		// 		.catch(console.error)
-		// }
+			var jsonFile = pointCloudEntry.fields.jsonFile;
+			var csvFile = pointCloudEntry.fields.csvFile;
 
-		if(csvFile){
-			client.getAsset(csvFile.sys.id)
-				.then(function(csvFileAsset){ console.log(csvFileAsset);
+			// if(jsonFile){
+			// 	client.getAsset(jsonFile.sys.id)
+			// 		.then(function(jsonFileAsset){ 
+			// 			console.log(jsonFileAsset);
+			// 			pointCloudData.jsonUrl = jsonFileAsset.fields.file.url;
+			// 			appendPointCloudToDOM(pointCloudData);
+			// 		})
+			// 		.catch(console.error)
+			// }
 
-					getCSV(csvFileAsset.fields.file.url, function(csvString){ 
+			if(csvFile){
+				client.getAsset(csvFile.sys.id)
+					.then(function(csvFileAsset){ console.log(csvFileAsset);
+
+						getCSV(csvFileAsset.fields.file.url, function(csvString){ 
+							
+							pointCloudData.rawJson = csvJSON(csvString);
+							appendPointCloudToDOM(pointCloudData);
+						});
+
 						
-						pointCloudData.rawJson = csvJSON(csvString);
-						appendPointCloudToDOM(pointCloudData);
-					});
+					})
+					.catch(console.error)
+			}
 
-					
-				})
-				.catch(console.error)
+		}).catch(console.error)
+	}
+	else{
+		// random point clouds periodically -- later interaction maybe
+
+		//var csv is the CSV file with headers
+		var randomJSON = function(){
+
+		  var pointCount = 1000000;//300000;
+
+		  var result = [];
+
+		  var headers=["X","Y","Z","SIGNAL_STRENGTH"];
+		  var headersMax=[30,300,200,99]; //TODO same negative range
+
+		  for(var i=1;i<pointCount;i++){
+
+			  var obj = {};
+
+			  for(var j=0;j<headers.length;j++){
+				  obj[headers[j]] = parseInt(getRandomInt(headersMax[j]));
+			  }
+
+			  result.push(obj);
+
+		  }
+		  
+		  //return result; //JavaScript object
+		  return JSON.stringify(result); //JSON
 		}
 
-	}).catch(console.error)
-}
+		var colorPresets=[
+			"Cool",
+			"Jet",
+			"Hot",
+			"Terrain",
+			"Grey"
+			];
+		var randomColorPreset = colorPresets[getRandomInt(colorPresets.length)];
+
+		var randomPointCloudData = {
+							scaleX: 82,
+							scaleY: 42,
+							scaleZ: 105,
+							positionX: -16,
+							positionY: -16,
+							positionZ: -40,
+						};
+
+		randomPointCloudData.rawJson = randomJSON();
+		appendPointCloudToDOM(randomPointCloudData, randomColorPreset);
+	}
+};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	document.querySelector('a-scene').addEventListener('loaded', function () {
+	    loadPointCloud();
+	});
+  });
