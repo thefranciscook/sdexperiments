c3dfd43b4b2c854840d6a9c0d877f64bc2a2e592
Szilárd Szakács <szakacsszilard@gmail.com>
2019-05-13 23:00:12 -0400

    model viewer added

--- seller1/js/config.js
+++ seller1/js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "SimpleStore",
+		brand : "Super Seller Store",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
